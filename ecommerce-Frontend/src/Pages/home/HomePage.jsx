a9a9f53545551701acffdd1af20fd87314b9249e
RaghavSaxena-eng <raghavsaxena91@gmail.com>
2026-02-17 19:53:47 +0530

    used render site for backend

--- ecommerce-Frontend/src/Pages/home/HomePage.jsx
+++ ecommerce-Frontend/src/Pages/home/HomePage.jsx
@@ -10,7 +10,7 @@ export function HomePage({cart, loadCart}) {
 
     useEffect ( () => {
         const getHomeData = async () => {
-            const response = await axios.get('/api/products')
+            const response = await axios.get('https://e-commerce-website-yw07.onrender.com/')
             setProducts(response.data);
         };
         getHomeData();
